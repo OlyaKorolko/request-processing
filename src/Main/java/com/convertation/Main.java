054e546a19eb31974889e257bebd2e769647e2ca
OlyaKorolko <71021414+OlyaKorolko@users.noreply.github.com>
2020-10-04 00:42:55 +0300

    Update Main.java

--- src/Main/java/com/convertation/Main.java
+++ src/Main/java/com/convertation/Main.java
@@ -1,39 +1,39 @@
 package com.convertation;
 
 import java.io.*;
+import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
-            File logFile = Processor.createFile(args[0]);
-            Processor processor = null;
+            if (args.length >= 3) {
+                File logFile = Processor.createFile(args[0]);
+                Processor processor = new Processor(scan, logFile);
 
-            try {
-                processor = new Processor(scan, logFile);
-            } catch (IOException ex) {
-                System.out.println("Log file is corrupted.");
-            }
-
-            if (processor != null) {
                 try (BufferedReader in = new BufferedReader(
                         new FileReader(Processor.createFile(args[1])))) {
-                    processor.setHolders(processor.deserializeToListOfStructureHolders(in));
+                    processor.setHolders(processor.deserializeToListOfCompanyStructures(in));
                 } catch (IOException e) {
                     processException("Input file is corrupted. Program is terminated");
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    processException("Input file is empty. Program is terminated");
                 } catch (NumberFormatException | DateTimeParseException ex) {
                     processException("Obligatory fields are empty. Program is terminated");
                 }
 
-                if (processor.getHolders() != null) {
+                if (processor.getHolders().size() != 0) {
                     try (BufferedWriter out = new BufferedWriter(
                             new FileWriter(Processor.createFile(args[2])))) {
                         showMenu();
-                        processor.processRequest(processor.getHolders(), scan, out);
+                        int input;
+                        do {
+                            System.out.println("Enter the number of request: ");
+                            input = scan.nextInt();
+                            processRequest(scan, out, processor, input);
+                        } while (input != 6);
                     } catch (IOException ex) {
                         processException("Output file is corrupted. Program is terminated");
                     } catch (InputMismatchException ex) {
@@ -41,8 +41,8 @@ public class Main {
                     }
                 }
             }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        } catch (CustomException e) {
+            e.printStackTrace();
         }
     }
 
@@ -59,5 +59,105 @@ public class Main {
     public static void processException(String s) {
         System.out.println(s);
         Processor.logger.writeExceptionToLogFile(s);
+    }
+
+    public static void processRequest(Scanner scan, BufferedWriter out, Processor processor, int type)
+            throws InputMismatchException {
+        switch (type) {
+            case 1: {
+                System.out.println("Enter a short name: ");
+                scan.nextLine();
+                String input = scan.nextLine().toLowerCase();
+                try {
+                    CompanyStructure s = processor.searchByShortName(input);
+                    Processor.logger.writeRequestDataToLogFile(1, input, s != null ? 1 : 0);
+                    if (s != null) {
+                        out.write(s.toString());
+                    }
+                    out.write("\n");
+                } catch (IOException ex) {
+                    System.out.println("No field found according to your request");
+                }
+                break;
+            }
+            case 2: {
+                System.out.println("Branch of work: ");
+                scan.nextLine();
+                String branchOfWork = scan.nextLine();
+                try {
+                    List<CompanyStructure> listOfSh = processor.searchByBranchOrTypeOfWork(branchOfWork, true);
+                    Processor.logger.writeRequestDataToLogFile(2, branchOfWork, listOfSh.size());
+                    for (CompanyStructure s : listOfSh) {
+                        out.write(s.toString());
+                    }
+                    out.write("\n");
+                } catch (IOException ex) {
+                    System.out.println("No field found according to your request");
+                }
+                break;
+            }
+            case 3: {
+                System.out.println("Type of work: ");
+                scan.nextLine();
+                String typeOfWork = scan.nextLine();
+                try {
+                    List<CompanyStructure> listOfSh = processor.searchByBranchOrTypeOfWork(typeOfWork, false);
+                    Processor.logger.writeRequestDataToLogFile(3, typeOfWork, listOfSh.size());
+                    for (CompanyStructure s : listOfSh) {
+                        out.write(s.toString());
+                    }
+                    out.write("\n");
+                } catch (IOException ex) {
+                    System.out.println("No field found according to your request");
+                }
+                break;
+            }
+            case 4: {
+                System.out.println("Enter the time boundaries to be searched between:");
+                String[] dateIn = {scan.next(), scan.next()};
+                try {
+                    List<CompanyStructure> listOfSh = processor.searchByDate(
+                            LocalDate.parse(dateIn[0]), LocalDate.parse(dateIn[1]));
+                    Processor.logger.writeRequestDataToLogFile(4, Arrays.toString(dateIn), listOfSh.size());
+                    for (CompanyStructure s : listOfSh) {
+                        out.write(s.toString());
+                    }
+                    out.write("\n");
+                } catch (IOException ex) {
+                    System.out.println("No field found according to your request");
+                } catch (DateTimeParseException ex) {
+                    System.out.println("Incorrect date format");
+                }
+                break;
+            }
+            case 5: {
+                System.out.println("Enter the lower and upper bounds of number of employees: ");
+                String[] emIn = {scan.next(), scan.next()};
+                try {
+                    List<CompanyStructure> listOfSh = processor.searchByEmployees(
+                            Integer.parseInt(emIn[0]), Integer.parseInt(emIn[1]));
+                    Processor.logger.writeRequestDataToLogFile(
+                            5, Arrays.toString(emIn), listOfSh.size());
+                    for (CompanyStructure s : listOfSh) {
+                        out.write(s.toString());
+                    }
+                    out.write("\n");
+                } catch (IOException ex) {
+                    System.out.println("No field found according to your request");
+                } catch (NumberFormatException ex) {
+                    System.out.println("Invalid input.");
+                }
+                break;
+            }
+            case 6: {
+                Processor.logger.writeRequestDataToLogFile(6,
+                        "REQUEST HANDLING FINISHED SUCCESSFULLY.", 0);
+                break;
+            }
+            default: {
+                System.out.println("Invalid request.");
+                break;
+            }
+        }
     }
 }
