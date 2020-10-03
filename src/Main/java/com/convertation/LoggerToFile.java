415ef6df462f3305aadb4d118a35912a9a4500ac
OlyaKorolko <71021414+OlyaKorolko@users.noreply.github.com>
2020-10-03 22:18:07 +0300

    Update LoggerToFile.java

--- src/Main/java/com/convertation/LoggerToFile.java
+++ src/Main/java/com/convertation/LoggerToFile.java
@@ -8,54 +8,62 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class LoggerToFile {
-    private final static Logger LOGGER = Logger.getLogger(Processor.class.getName());
+    private static Logger LOGGER;
     private static final SimpleFormatter formatter = new SimpleFormatter();
 
-    LoggerToFile(File fileName) throws IOException {
+    LoggerToFile(File fileName, String className) throws CustomException {
+        LOGGER = Logger.getLogger(className);
         LOGGER.setLevel(Level.FINE);
-        FileHandler filehandler = new FileHandler(fileName.getName(), true);
-        filehandler.setLevel(Level.FINE);
-        filehandler.setFormatter(formatter);
-        LOGGER.addHandler(filehandler);
-        LOGGER.fine("REQUEST HANDLING START" + "\n");
+        try {
+            FileHandler filehandler = new FileHandler(fileName.getName(), true);
+            filehandler.setLevel(Level.FINE);
+            filehandler.setFormatter(formatter);
+            LOGGER.addHandler(filehandler);
+            LOGGER.fine("REQUEST HANDLING START" + "\n");
+        } catch (IOException ex) {
+            throw new CustomException("Log file is corrupted.", ex);
+        }
     }
 
-    public void writeRequestDataToLogFile(int requestNum, String input, int result) {
-        switch (requestNum) {
-            case 1: {
+    public void writeRequestDataToLogFile(Types requestType, String requestInput, int result) {
+        switch (requestType) {
+            case SHORT_NAME: {
                 LOGGER.fine("Find a company by short name | short name: " +
-                        input + ", companies found: " + result + "\n");
+                        requestInput + ", companies found: " + result + "\n");
                 break;
             }
-            case 2: {
+            case BRANCH_OF_WORK: {
                 LOGGER.fine("Find companies by branch of work | branch: " +
-                        input + ", companies found: " + result + "\n");
+                        requestInput + ", companies found: " + result + "\n");
                 break;
             }
-            case 3: {
+            case TYPE_OF_WORK: {
                 LOGGER.fine("Find companies by type of work | type: " +
-                        input + ", companies found: " + result + "\n");
+                        requestInput + ", companies found: " + result + "\n");
                 break;
             }
-            case 4: {
-                String[] in = input.split(" ");
+            case DATE: {
+                String[] in = requestInput.split(" ");
                 LOGGER.fine("Find companies by foundation date | date: " +
                         in[0] + in[1] + ", companies found: " + result + "\n");
                 break;
             }
-            case 5: {
-                String[] in = input.subSequence(1, input.length() - 1).toString().split(", ");
-                LOGGER.fine("Find companies by employees number | number: " + "[" + Long.parseLong(in[0]) +
-                        "," + Long.parseLong(in[1]) + "]" + ", companies found: " + result + "\n");
+            case EMPLOYEES: {
+                String[] in = requestInput.subSequence(1, requestInput.length() - 1).toString().split(", ");
+                LOGGER.fine("Find companies by employees number | number: " + "[" + Integer.parseInt(in[0]) +
+                        "," + Integer.parseInt(in[1]) + "]" + ", companies found: " + result + "\n");
                 break;
             }
-            case 6: {
-                LOGGER.fine(input + "\n");
+            case EXIT: {
+                LOGGER.fine(requestInput + "\n");
+            }
+            default: {
+                LOGGER.fine("Invalid request.");
             }
         }
     }
 
     public void writeExceptionToLogFile(String s) {
-        LOGGER.warning(s + "\n");
+        LOGGER.severe(s + "\n");
     }
 }
