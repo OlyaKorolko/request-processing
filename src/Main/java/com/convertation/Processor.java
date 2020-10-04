91299828d783aa636597a98140e20af94b029e22
OlyaKorolko <71021414+OlyaKorolko@users.noreply.github.com>
2020-10-04 13:45:18 +0300

    Update Processor.java
    вынесла исключения в мэйн

--- src/Main/java/com/convertation/Processor.java
+++ src/Main/java/com/convertation/Processor.java
@@ -1,87 +1,143 @@
 package com.convertation;
 
-import java.io.BufferedReader;
-import java.io.File;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            if (args.length >= 3) {
+                File logFile = Processor.createFile(args[0]);
+                Processor processor = new Processor(logFile);
 
-public class Processor {
-    public static LoggerToFile logger;
-    private Map<String, CompanyStructure> holders;
+                try (BufferedReader in = new BufferedReader(
+                        new FileReader(Processor.createFile(args[1])))) {
+                    processor.setHolders(processor.deserializeToListOfCompanyStructures(in));
+                } catch (IOException e) {
+                    processException("Input file is corrupted. Program is terminated");
+                } catch (NumberFormatException | DateTimeParseException ex) {
+                    processException("Obligatory fields are empty. Program is terminated");
+                }
 
-    Processor(File logFile) throws CustomException {
-        holders = new HashMap<>();
-        logger = new LoggerToFile(logFile, Processor.class.getName());
-    }
-
-    public void setHolders(Map<String, CompanyStructure> holders) {
-        this.holders = holders;
-    }
-
-    public Map<String, CompanyStructure> getHolders() {
-        return holders;
-    }
-
-    public static File createFile(String fileName) {
-        return new File(System.getProperty("user.dir") + "\\src\\" + fileName);
-    }
-
-    public Map<String, CompanyStructure> deserializeToListOfCompanyStructures(BufferedReader in)
-            throws ArrayIndexOutOfBoundsException {
-        return in.lines().map(line -> {
-            String[] x = setPattern().split(line);
-            return new CompanyStructure(x[0], x[1], LocalDate.parse(x[2]), x[3], LocalDate.parse(x[4]),
-                    Integer.parseInt(x[5]), x[6], x[7], x[8], x[9], x[10], x[11]);
-        })
-                .filter(s -> !s.getShortName().isEmpty() || !s.getBranchOfWork().isEmpty() || !s.getTypeOfWork().isEmpty())
-                .collect(Collectors.toMap(CompanyStructure::getShortNameToLowerCase, Function.identity()));
-    }
-
-    private Pattern setPattern() {
-        return Pattern.compile(",");
-    }
-
-    CompanyStructure searchByShortName(String shortName) {
-        return holders.get(shortName);
-    }
-
-    List<CompanyStructure> searchByBranchOrTypeOfWork(String branchType, boolean choice) {
-        if (choice) {
-            return holders
-                    .values()
-                    .stream()
-                    .filter(sh -> sh.getBranchOfWork().equals(branchType))
-                    .collect(Collectors.toList());
-        } else {
-            return holders
-                    .values()
-                    .stream()
-                    .filter(sh -> sh.getTypeOfWork().equals(branchType))
-                    .collect(Collectors.toList());
+                if (processor.getHolders().size() != 0) {
+                    try (BufferedWriter out = new BufferedWriter(
+                            new FileWriter(Processor.createFile(args[2])))) {
+                        showMenu();
+                        int input;
+                        do {
+                            System.out.println("Enter the number of request: ");
+                            input = scan.nextInt();
+                            processRequest(scan, out, processor, RequestName.values()[input - 1]);
+                        } while (RequestName.values()[input - 1] != RequestName.EXIT);
+                    } catch (IOException ex) {
+                        processException("Output file is corrupted. Program is terminated");
+                    } catch (InputMismatchException | NumberFormatException ex) {
+                        processException("Request is invalid. Program is terminated");
+                    } catch (DateTimeParseException ex) {
+                        processException("Incorrect date format");
+                    }
+                }
+            }
+        } catch (CustomException e) {
+            e.printStackTrace();
         }
     }
 
-    List<CompanyStructure> searchByDate(LocalDate d1, LocalDate d2)
-            throws DateTimeParseException {
-        return holders
-                .values()
-                .stream()
-                .filter(sh -> sh.getDateOfActualization().isAfter(d1) && sh.getDateOfActualization().isBefore(d2))
-                .collect(Collectors.toList());
+    public static void showMenu() {
+        System.out.println("Here are all the available requests: ");
+        System.out.println("1. Find a company by short name");
+        System.out.println("2. Find companies by branch of work");
+        System.out.println("3. Find companies by type of work");
+        System.out.println("4. Find companies by foundation date (between DATE1 && DATE2) [YYYY-MM-DD]");
+        System.out.println("5. Find companies by employees number (between NUM1 && NUM2)");
+        System.out.println();
     }
 
-    List<CompanyStructure> searchByEmployees(Integer lower, Integer upper) {
-        return holders
-                .values()
-                .stream()
-                .filter(sh -> sh.getNumberOfEmployees() >= lower && sh.getNumberOfEmployees() <= upper)
-                .collect(Collectors.toList());
+    public static void processException(String s) {
+        System.out.println(s);
+        Processor.logger.writeExceptionToLogFile(s);
+    }
+
+    public static void processRequest(Scanner scan, BufferedWriter out, Processor processor, RequestName requestName)
+            throws IOException {
+        switch (requestName) {
+            case SHORT_NAME: {
+                System.out.println("Enter a short name: ");
+                scan.nextLine();
+                String input = scan.nextLine().toLowerCase();
+                CompanyStructure s = processor.searchByShortName(input);
+                Processor.logger.writeRequestDataToLogFile(RequestName.SHORT_NAME, input, s != null ? 1 : 0);
+                if (s != null) {
+                    out.write(s.toString());
+                }
+                out.write("\n");
+            }
+            break;
+            case BRANCH_OF_WORK: {
+                System.out.println("Branch of work: ");
+                scan.nextLine();
+                String branchOfWork = scan.nextLine();
+                List<CompanyStructure> listOfSh = processor.searchByBranchOrTypeOfWork(branchOfWork, true);
+                Processor.logger.writeRequestDataToLogFile(RequestName.BRANCH_OF_WORK, branchOfWork, listOfSh.size());
+                for (CompanyStructure s : listOfSh) {
+                    out.write(s.toString());
+                }
+                out.write("\n");
+                break;
+            }
+            case TYPE_OF_WORK: {
+                System.out.println("Type of work: ");
+                scan.nextLine();
+                String typeOfWork = scan.nextLine();
+                List<CompanyStructure> listOfSh = processor.searchByBranchOrTypeOfWork(typeOfWork, false);
+                Processor.logger.writeRequestDataToLogFile(RequestName.TYPE_OF_WORK, typeOfWork, listOfSh.size());
+                for (CompanyStructure s : listOfSh) {
+                    out.write(s.toString());
+                }
+                out.write("\n");
+                break;
+            }
+            case DATE: {
+                System.out.println("Enter the time boundaries to be searched between:");
+                String[] dateIn = {scan.next(), scan.next()};
+                List<CompanyStructure> listOfSh = processor.searchByDate(
+                        LocalDate.parse(dateIn[0]), LocalDate.parse(dateIn[1]));
+                Processor.logger.writeRequestDataToLogFile(
+                        RequestName.DATE, Arrays.toString(dateIn), listOfSh.size());
+                for (CompanyStructure s : listOfSh) {
+                    out.write(s.toString());
+                }
+                out.write("\n");
+                break;
+            }
+            case EMPLOYEES: {
+                System.out.println("Enter the lower and upper bounds of number of employees: ");
+                String[] emIn = {scan.next(), scan.next()};
+                List<CompanyStructure> listOfSh = processor.searchByEmployees(
+                        Integer.parseInt(emIn[0]), Integer.parseInt(emIn[1]));
+                Processor.logger.writeRequestDataToLogFile(
+                        RequestName.EMPLOYEES, Arrays.toString(emIn), listOfSh.size());
+                for (CompanyStructure s : listOfSh) {
+                    out.write(s.toString());
+                }
+                out.write("\n");
+
+                break;
+            }
+            case EXIT: {
+                Processor.logger.writeRequestDataToLogFile(RequestName.EXIT,
+                        "REQUEST HANDLING FINISHED SUCCESSFULLY.", 0);
+                break;
+            }
+            default: {
+                System.out.println("Invalid request.");
+                break;
+            }
+        }
     }
 }
