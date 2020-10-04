43da070932a24e0b7ffea5553026084862abaf02
OlyaKorolko <71021414+OlyaKorolko@users.noreply.github.com>
2020-10-04 15:20:38 +0300

    Update CustomException.java

--- src/Main/java/com/convertation/CustomException.java
+++ src/Main/java/com/convertation/CustomException.java
@@ -1,12 +1,11 @@
 package com.convertation;
 
-import java.io.IOException;
-
 public class CustomException extends Exception {
-    public CustomException(String errorMessage, IOException err) {
-        super(errorMessage, err);
+    public CustomException(String message) {
+        super(message);
     }
-    public CustomException(String errorMessage, ArrayIndexOutOfBoundsException err) {
-        super(errorMessage, err);
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
